21b96bf02bc85551cde01282d60054b7ed4c54e8
Juan-Restrepo-B <juanprestrepob@gmail.com>
2024-02-24 19:52:09 -0500

    Se realiza la integracion del log usuario queda incompleto

---
+++ BACKEND/PROCESO_INICIAL/REGISTRO_USUARIO/src/main/java/eduessence/registro_cliente/models/dao/ILogNegocioDao.java
@@ -0,0 +1,7 @@
+package eduessence.registro_cliente.models.dao;
+
+import eduessence.registro_cliente.models.entity.LogNegocio;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ILogNegocioDao extends JpaRepository<LogNegocio, Long> {
+}
