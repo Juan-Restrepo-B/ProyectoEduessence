0500a9d79b78f8d088cf01c1f8f0a2c3bb4c0e10
Juan-Restrepo-B <juanprestrepob@gmail.com>
2024-03-16 00:17:15 -0500

    -Se realiza la creacion de los Exceeption para el Back-End Inicio de Sesion -Hace falta la creacion del Back-End del envio de los correos para el Registro de Uusario

---
+++ BACKEND/PROCESO_INICIAL/INICIAR_SESION/src/main/java/eduessence/iniciar_sesion/models/exception/RequestException.java
@@ -0,0 +1,12 @@
+package eduessence.iniciar_sesion.models.exception;
+
+import lombok.Data;
+
+@Data
+public class RequestException extends RuntimeException{
+    private String code;
+    public RequestException(String code, String message) {
+        super(message);
+        this.code = code;
+    }
+}
