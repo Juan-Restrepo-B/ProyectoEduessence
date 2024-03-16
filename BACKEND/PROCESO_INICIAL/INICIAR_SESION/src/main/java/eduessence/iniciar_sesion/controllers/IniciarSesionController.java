c6f1f66176daa208b833055b0c71b7e0d8f61b2d
Juan-Restrepo-B <juanprestrepob@gmail.com>
2024-03-16 00:20:17 -0500

    -Se realiza la creacion de los Controller para el Back-End Inicio de Sesion -Hace falta la creacion del Back-End del envio de los correos para el Registro de Uusario

---
+++ BACKEND/PROCESO_INICIAL/INICIAR_SESION/src/main/java/eduessence/iniciar_sesion/controllers/IniciarSesionController.java
@@ -0,0 +1,12 @@
+package eduessence.iniciar_sesion.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api/iniciar_seseion")
+public class IniciarSesionController {
+
+}
